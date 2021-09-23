de017d97efae439759f3866b4842fe9a0f68876a
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-23 21:48:08 +0000

    fixed betty

--- utilities.c
+++ utilities.c
@@ -17,7 +17,11 @@ int check_num(char *token)
 	}
 	return (1);
 }
-
+/**
+ *error_handler - prints to stderr
+ *@line_count: count of lines
+ *@line: line of file to be freed
+ */
 void error_handler(int line_count, char *line)
 {
 	fprintf(stderr, "L%d: usage: push integer\n", line_count);
