b51162d541b2d13a7cfff977b41bb2b755fecb01
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-27 02:20:33 +0000

    added div by 0 error msg

--- nodeutil3.c
+++ nodeutil3.c
@@ -38,6 +38,11 @@ void division(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	else
 	{
 		temp = *stack;
