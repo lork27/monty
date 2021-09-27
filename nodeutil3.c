8d7b2c90a5143f85222d2ce823c8809840c8946a
lork27 <g.lorca.lamadrid@gmail.com>
2021-09-27 02:01:41 +0000

    added file with sub and div functions

---
+++ nodeutil3.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+/*File for the advanced tasks functions*/
+
+/**
+ *sub - sub the top two element from the stack
+ *@stack: sequencial data structure
+ *@line_number: line num
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		temp = *stack;
+		*stack = temp->next;
+		(*stack)->n = temp->n - (*stack)->n;
+		(*stack)->prev = temp->prev;
+		free(temp);
+	}
+}
+/**
+ *division - divides top two elements from stack
+ *@stack: the stack of nodes
+ *@line_number: line number from which div was called
+ */
+void division(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		temp = *stack;
+		*stack = temp->next;
+		(*stack)->n = temp->n / (*stack)->n;
+		(*stack)->prev = temp->prev;
+		free(temp);
+	}
+}
